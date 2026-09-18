cee91e492c19f52eac2218a7db8026efc1dac812
akhilrattan <akhilrattan3@gmail.com>
2026-09-19 04:39:20 +0530

    added memory

---
+++ app/memory/memory.py
@@ -0,0 +1,67 @@
+from db.database import Database
+from pgvector.psycopg import register_vector
+
+from rag.embedder import Embedder
+
+
+class Memory:
+
+    def __init__(self, embedder: Embedder):
+
+        self.db = Database()
+        self.embedder = embedder
+
+    def save(self, content:str):
+        embeddings = self.embedder.embed([content])[0]
+
+        with self.db.connect() as connection:
+            register_vector(connection)
+
+            connection.execute(
+                """
+                INSERT INTO memories
+                (content,embeddings)
+                VALUES(%s,%s)
+                """,
+                (content,embeddings)
+            )
+            connection.commit()
+    
+    def search(self,query:str,top_k:int)->list[str]:
+        query_embeddings = self.embedder.embed([query])[0]
+
+        with self.db.connect() as connection:
+            register_vector(query_embeddings)
+            cursor = connection.execute(
+                """
+                SELECT content 
+                FROM memories
+                ORDER embedding <=> %s
+                LIMIT %s
+                """,
+                (query_embeddings,top_k)
+            )
+            rows = cursor.fetchall()
+        return [
+            row[0]
+            for row in rows
+        ]
+
+    def get_all(self) -> list[str]:
+
+        with self.db.connect() as connection:
+
+            cursor = connection.execute(
+                """
+                SELECT content
+                FROM memories
+                ORDER BY id
+                """
+            )
+
+            rows = cursor.fetchall()
+
+        return [
+            row[0]
+            for row in rows
+        ]
